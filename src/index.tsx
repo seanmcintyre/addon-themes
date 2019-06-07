dfe9070f3d48bf661ab2b37c09c21a97179b386b
Sean McIntyre <sean.mcintyre@vimeo.com>
2019-06-06 20:11:18 -0400

    consolidate and simplify

--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,19 @@ import {
   StoryGetter,
 } from '@storybook/addons';
 import { ThemeProvider } from 'styled-components';
-import { theme } from './themeStore';
+import { getCurrentTheme } from './ThemeSwitcher';
 
 export const withThemes = makeDecorator({
   name: 'themes',
   parameterName: 'themes',
   skipIfNoParametersOrOptions: false,
   allowDeprecatedUsage: false,
-  wrapper: (getStory: StoryGetter, context: StoryContext) => (
-    <ThemeProvider theme={theme()}>{getStory(context)}</ThemeProvider>
-  ),
+  wrapper: (getStory: StoryGetter, context: StoryContext) => {
+    const theme = getCurrentTheme();
+    return (
+      <ThemeProvider theme={theme}>{getStory(context)}</ThemeProvider>
+    );
+  },
 });
 
-export { addThemes } from './themeStore';
+export { addThemes } from './ThemeSwitcher';
